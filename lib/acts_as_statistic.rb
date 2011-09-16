277f4957ba67fd1e3aa6746d8bb8cda215dd1418
Nick Jones <darellik@gmail.com>
2011-09-15 21:28:00 -0500

    Removing old README and cleaning up a bit.

--- lib/acts_as_statistic.rb
+++ lib/acts_as_statistic.rb
@@ -1,1 +1,8 @@
-# ActsAsStatistic
+require "active_record"
+
+require "acts_as_statistic/measured_statistic"
+
+
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend ActAsStatistic::MeasurableModel
+end
