3e3318670392b61aac0d390f43193cb679f2f496
Nick Jones <darellik@gmail.com>
2011-10-09 22:07:49 -0500

    Adjusting naming.

--- lib/acts_as_statistic.rb
+++ lib/acts_as_statistic.rb
@@ -2,7 +2,6 @@ require "active_record"
 
 require "acts_as_statistic/measured_statistic"
 
-
 if defined?(ActiveRecord::Base)
-  ActiveRecord::Base.extend ActAsStatistic::MeasurableModel
+  ActiveRecord::Base.extend ActsAsStatistic::MeasurementMethods
 end
