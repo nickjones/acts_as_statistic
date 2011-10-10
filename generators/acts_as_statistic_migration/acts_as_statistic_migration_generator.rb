865ae660e48ac21743b992b2e600896c83800e5f
Nick Jones <darellik@gmail.com>
2011-10-09 21:59:05 -0500

    Fixed a typo in the class name for migration.

--- generators/acts_as_statistic_migration/acts_as_statistic_migration_generator.rb
+++ generators/acts_as_statistic_migration/acts_as_statistic_migration_generator.rb
@@ -1,4 +1,4 @@
-class ActAsStatisticMigrationGenerator < Rails::Generator::Base
+class ActsAsStatisticMigrationGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => 'acts_as_statistic_migration'
